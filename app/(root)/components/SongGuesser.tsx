c422fe1048f2ef0e54fb65f765667d51c68339fb
racerooni <107811462+racerooni@users.noreply.github.com>
2024-01-14 19:51:30 +0100

    styles, more maps added

--- app/(root)/components/SongGuesser.tsx
+++ app/(root)/components/SongGuesser.tsx
@@ -18,7 +18,8 @@ const SongGuesser: React.FC<SongGuesserProps> = ({ songUrl, mapName }) => {
     return Songs[Math.floor(Math.random() * Songs.length)];
   });
   const [guessedMap, setGuessedMap] = useState("");
-
+  const [correctGuess, setCorrectGuess] = useState(0);
+  const [wrongGuess, setWrongGuess] = useState(0);
   const checkIfCorrect = () => {
     if (mapToGuess.mapName.includes(guessedMap)) {
       toast.success("you got it");
@@ -26,16 +27,25 @@ const SongGuesser: React.FC<SongGuesserProps> = ({ songUrl, mapName }) => {
       setMapToGuess(() => {
         return Songs[Math.floor(Math.random() * Songs.length)];
       });
+      setCorrectGuess(correctGuess + 1);
     } else {
       toast.error("try again");
       setGuessedMap("");
+      setWrongGuess(wrongGuess + 1);
     }
+  };
+  const skip = () => {
+    setMapToGuess(() => {
+      return Songs[Math.floor(Math.random() * Songs.length)];
+    });
   };
 
   return (
     <>
-      <div className="h-1/2 space-y-4 flex flex-col w-1/2">
-        <h1 className="mx-auto text-3xl">Guess the map!</h1>
+      <div className="min-h-1/2 flex flex-col w-1/2 bg-white bg-opacity-20 rounded-md px-6 gap-2.5 py-6">
+        <h1 className="mx-auto text-4xl text-white font-bold">
+          Guess the map!
+        </h1>
         <YouTube className="mx-auto" videoId={mapToGuess.songUrl} />
         <Input
           type="text"
@@ -46,6 +56,14 @@ const SongGuesser: React.FC<SongGuesserProps> = ({ songUrl, mapName }) => {
           }}
         />
         <Button onClick={checkIfCorrect}>Guess</Button>
+        <Button variant="destructive" onClick={skip}>
+          Skip
+        </Button>
+        <div className="text-white">
+          Correct guesses:{" "}
+          <span className="text-green-500">{correctGuess}</span> <br />
+          Wrong guesses: <span className="text-red-500">{wrongGuess}</span>
+        </div>
       </div>
     </>
   );
