5b35185af3e3ca0be4bad829c622346d2cce3823
racerooni <107811462+racerooni@users.noreply.github.com>
2024-01-14 20:32:43 +0100

    more maps added

--- app/(root)/components/SongGuesser.tsx
+++ app/(root)/components/SongGuesser.tsx
@@ -21,20 +21,23 @@ const SongGuesser: React.FC<SongGuesserProps> = ({ songUrl, mapName }) => {
   const [correctGuess, setCorrectGuess] = useState(0);
   const [wrongGuess, setWrongGuess] = useState(0);
   const checkIfCorrect = () => {
-    if (mapToGuess.mapName.includes(guessedMap)) {
-      toast.success("you got it");
+    if (mapToGuess.mapName.includes(guessedMap.toLowerCase())) {
+      toast.success("You got it!");
       setGuessedMap("");
       setMapToGuess(() => {
         return Songs[Math.floor(Math.random() * Songs.length)];
       });
       setCorrectGuess(correctGuess + 1);
     } else {
-      toast.error("try again");
+      toast.error("Try again!");
       setGuessedMap("");
       setWrongGuess(wrongGuess + 1);
     }
   };
   const skip = () => {
+    toast(`The map was: ${mapToGuess.mapName[0]}`, {
+      icon: "😂",
+    });
     setMapToGuess(() => {
       return Songs[Math.floor(Math.random() * Songs.length)];
     });
@@ -42,11 +45,11 @@ const SongGuesser: React.FC<SongGuesserProps> = ({ songUrl, mapName }) => {
 
   return (
     <>
-      <div className="min-h-1/2 flex flex-col w-1/2 bg-white bg-opacity-20 rounded-md px-6 gap-2.5 py-6">
+      <div className="min-h-1/2 flex flex-col w-1/2 bg-white bg-opacity-20 rounded-md px-6 gap-2.5 py-6 relative">
         <h1 className="mx-auto text-4xl text-white font-bold">
-          Guess the map!
+          Guess the map based on it's song!
         </h1>
-        <YouTube className="mx-auto" videoId={mapToGuess.songUrl} />
+        <YouTube className="mx-auto w-100" videoId={mapToGuess.songUrl} />
         <Input
           type="text"
           placeholder="What map has this song?"
@@ -64,6 +67,9 @@ const SongGuesser: React.FC<SongGuesserProps> = ({ songUrl, mapName }) => {
           <span className="text-green-500">{correctGuess}</span> <br />
           Wrong guesses: <span className="text-red-500">{wrongGuess}</span>
         </div>
+        <p className="absolute bottom-0 right-0 text-white text-xs text-right">
+          thanks to niim0te🍐 for picking the songs. <br /> made by racer 😚😚
+        </p>
       </div>
     </>
   );
