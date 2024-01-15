8ff063eeab46dabfcd724497e3af2fd88ed65a7f
racerooni <107811462+racerooni@users.noreply.github.com>
2024-01-15 13:37:35 +0100

    i dont know

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,14 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body
-          className={`${inter.className} flex w-full h-screen justify-center items-center bg-[url('https://i.imgur.com/xViLugU.png')] bg-no-repeat bg-center bg-cover`}
-        >
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body
+        className={`${inter.className} flex w-full h-screen justify-center items-center bg-[url('https://i.imgur.com/xViLugU.png')] bg-no-repeat bg-center bg-cover`}
+      >
+        {children}
+      </body>
+    </html>
   );
 }
